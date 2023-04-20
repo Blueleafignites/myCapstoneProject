53d198e8e56d90180b04e32b9411c3487154a8d2
Rachel Mettler <bluemettler@gmail.com>
2023-04-20 10:10:16 -0700

    Worked on connecting to database

--- src/data.js
+++ src/data.js
@@ -1,69 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Lists from './Lists';
-const { Sequelize, Model, DataTypes } = require('sequelize');
-
-const sequelize = new Sequelize('raven_bujo', 'root', 'Mettler02?', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-class Priority extends Model {}
-Priority.init(
-  {
-    priority_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    priority_name: DataTypes.STRING,
-    priority_color: DataTypes.STRING,
-  },
-  { sequelize, modelName: 'Priority', tableName: 'priorities' },
-);
-
-class Tag extends Model {}
-Tag.init(
-  {
-    tag_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    tag_name: DataTypes.STRING,
-    tag_color: DataTypes.STRING,
-  },
-  { sequelize, modelName: 'Tag', tableName: 'tags' },
-);
-
-class List extends Model {}
-List.init(
-  {
-    list_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    list_name: DataTypes.STRING,
-  },
-  { sequelize, modelName: 'List', tableName: 'lists' },
-);
-
-class Task extends Model {}
-Task.init(
-  {
-    task_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    list_id: DataTypes.INTEGER,
-    priority_id: DataTypes.INTEGER,
-    tag_id: DataTypes.INTEGER,
-    task_title: DataTypes.STRING,
-    deadline: DataTypes.DATE,
-  },
-  { sequelize, modelName: 'Task', tableName: 'tasks' },
-);
 
 function Data() {
   const [priorities, setPriorities] = useState([]);
@@ -71,40 +8,24 @@ function Data() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    (async () => {
-      try {
-        const priorityData = await Priority.findAll({
-          attributes: ['priority_name', 'priority_color'],
-        });
-        setPriorities(priorityData);
-      } catch (error) {
-        console.error('Error retrieving priorities:', error);
-      }
-    })();
-  }, []);
+    const fetchPriorities = async () => {
+      const res = await axios.get('http://localhost:3000/priorities');
+      setPriorities(res.data);
+    };
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const tagData = await Tag.findAll({
-          attributes: ['tag_name', 'tag_color'],
-        });
-        setTags(tagData);
-      } catch (error) {
-        console.error('Error retrieving tags:', error);
-      }
-    })();
-  }, []);
+    const fetchTags = async () => {
+      const res = await axios.get('http://localhost:3000/tags');
+      setTags(res.data);
+    };
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const taskData = await Task.findAll();
-        setTasks(taskData);
-      } catch (error) {
-        console.error('Error retrieving tasks:', error);
-      }
-    })();
+    const fetchTasks = async () => {
+      const res = await axios.get('http://localhost:3000/tasks');
+      setTasks(res.data);
+    };
+
+    fetchPriorities();
+    fetchTags();
+    fetchTasks();
   }, []);
 
   return (
